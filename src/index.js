b7f09af63358b4923a2997cd7550421b1d489929
Akash Patil <patilakash1285@gmail.com>
2023-06-20 16:21:36 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { library } from '@fortawesome/fontawesome-svg-core'
@@ -17,4 +20,3 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
